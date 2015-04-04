80cacb02293d98039aa42cc8c264216297a83e05
Johan Hogberg <joholil@hotmail.com>
2015-04-04 11:01:24 +0200

    Innan användande av Core Data

--- Studie1/Studie1/assignmentViewController.swift
+++ Studie1/Studie1/assignmentViewController.swift
@@ -20,10 +20,6 @@ class assignmentViewController: UIViewController {
     
     var assignments:[Assignment] = [Assignment(headline: "Uppgift 1", descriptionText: "Som din första uppgift ska du gå bort till brödavdelningen. Där ska du leta reda på en gul skyllt märkt med Karlstads Universitet. För din telefon i närheten av skylten tills du får ett meddelande om att du har klarat uppgiften. Märkningen ser ut så här..."), Assignment(headline: "Uppgift 2", descriptionText: "Som din andra uppgift ska du gå bort till mjölkavdelning. Där ska du leta reda på en gul skyllt märkt med Karlstads Universitet. För din telefon i närheten av skylten tills du får ett meddelande om att du har klarat uppgiften. Märkningen ser ut så här..."), Assignment(headline: "Uppgift 3", descriptionText: "Som din tredje uppgift ska du gå bort till mjölavdelning. Där ska du leta reda på en gul skyllt märkt med Karlstads Universitet. För din telefon i närheten av skylten tills du får ett meddelande om att du har klarat uppgiften. Märkningen ser ut så här..."), Assignment(headline: "Uppgift 4", descriptionText: "Som din fjärde uppgift ska du gå bort till klädavdelning. Där ska du leta reda på en gul skyllt märkt med Karlstads Universitet. För din telefon i närheten av skylten tills du får ett meddelande om att du har klarat uppgiften. Märkningen ser ut så här..."), Assignment(headline: "Uppgift 5", descriptionText: "Som din femte uppgift ska du gå bort till godisavdelningen. Där ska du leta reda på en gul skyllt märkt med Karlstads Universitet. För din telefon i närheten av skylten tills du får ett meddelande om att du har klarat uppgiften. Märkningen ser ut så här...")]
     
-    /*
-    var assignment = Assignment(headline: "Uppgift 1", descriptionText: "Som din första  så här:", logo: "loggan", finishedText: "sluttexten", seconds: 0)
-    */
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,12 +45,14 @@ class assignmentViewController: UIViewController {
     
     @IBAction func finished()
     {
-        if currentAssignment == 4
+        if currentAssignment == assignments.count - 1
         {
+            if let finishedController = storyboard?.instantiateViewControllerWithIdentifier("finnishedView") as?UIViewController {presentViewController(finishedController, animated: true, completion: nil)
+            }
         }
         else
         {
-            
+            assignments[currentAssignment].endTime = CFAbsoluteTimeGetCurrent()
             currentAssignment = currentAssignment + 1;
             initializeAssignment()
         }
