8a3671872e38926fb795ebd65820d9891b2cff8c
Johan Hogberg <joholil@hotmail.com>
2015-11-05 09:12:52 +0100

    Incheckning

---
+++ Studie2/Studie2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Studie2
+//
+//  Created by johahogb on 05/11/15.
+//  Copyright © 2015 Service research center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
